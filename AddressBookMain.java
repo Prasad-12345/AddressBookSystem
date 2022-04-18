3ecb28d42b5de810d06306a908cc0d0581cf4723
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-18 14:21:23 +0530

    Added: Welcome message

---
+++ AddressBookMain.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.addressbook;
+/*
+ * Author: Prasad
+ * Ability to create contacts in address book with first and last names, address, city, state
+ * zip, phone number and email
+ */
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+	}
+}
