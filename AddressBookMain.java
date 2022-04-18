449700fe478389dcea962bc11f47d50bb7cc307d
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-04-18 17:47:40 +0530

    Refactored: UC4 Ability to delete a person using person's name

---
+++ AddressBookMain.java
@@ -0,0 +1,43 @@
+package com.bridgelabz.addressbook;
+
+import java.util.Scanner;
+
+/*
+ * Author: Prasad
+ * Ability to delete a person using persons name
+ */
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+		
+		AddressBook addressBook = new AddressBook();
+		addressBook.addContact(addressBook.contact);
+		addressBook.display();
+		
+		System.out.println("Do you want to edit contacts");
+		System.out.println("Enter yes to edit");
+		Scanner sc = new Scanner(System.in);
+		String input = sc.next();
+		
+		if(input.equals("yes") || input.equals("Yes")) {
+			addressBook.editContact();
+			addressBook.display();
+		}
+		else {
+			System.out.println("Thank You");
+		}
+		
+		System.out.println("Do you want to delete contacts");
+		System.out.println("Enter yes to delete");
+		input = sc.next();
+		
+		if(input.equals("yes") || input.equals("Yes")) {
+			addressBook.deleteContact();
+			addressBook.display();
+		}
+		else {
+			System.out.println("Thank You");
+		}
+	}
+}
