692dc01fdc3afbcd4d5aa910168b965e25f5e57b
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-05-11 12:54:44 +0530

    Added: Ability to search Person in a City or State across the multiple AddressBook

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -4,14 +4,60 @@ import java.util.Scanner;
 
 /*
  * Author: Prasad
- * Ability to ensure there is no Duplicate Entry of the same Person in a particular Address Book
+ * Ability to search Person in a City or State across the multiple AddressBook
  */
 public class AddressBookMain {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program");
 		//object
-		AddressBookOperation addressBookOperation = new AddressBookOperation();
-		addressBookOperation.operations();	
+		AddressBookMain addressBookMain = new AddressBookMain();
+		addressBookMain.operations();
 	}
-}
+
+	/*
+	 * This method contains all address book operation    
+	 */
+	public void operations() {
+		AddressBookOperation addressBookOperation = new AddressBookOperation();
+		Scanner sc = new Scanner(System.in);
+		//operations according to user choice
+		boolean check = true;
+		do {
+			System.out.println("Enter your choice");
+			System.out.println("1.Add Address Book\n2.Display Address Book\n3.Edit address book\n4.Delete address book\n5.Search person\n6.Exit");
+			int choice = sc.nextInt();
+			switch(choice) {
+				case 1:
+					addressBookOperation.addAddressBook();
+					break;
+					
+				case 2:
+					addressBookOperation.displayAddressBook();
+					break;
+					
+				case 3:
+					addressBookOperation.editAddressBook();
+					break;
+					
+				case 4:
+					addressBookOperation.deleteAddressBook();
+					break;
+						
+				case 5:
+					System.out.println("Enter \n1.Search By City\n2.Search By State");
+					int searchChoice = sc.nextInt();
+					if(searchChoice==1)
+						addressBookOperation.searchByCity();
+					else 
+						addressBookOperation.searchByState();
+						break;
+						
+				case 6: 
+					check = false;
+					break;
+					}
+				}
+				while(check);
+			}
+	}
